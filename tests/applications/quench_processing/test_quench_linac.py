8463dba332895219bcd2fb9c77046e8e19f23e29
Lisa Zacarias <zacarias@slac.stanford.edu>
2024-09-17 10:46:08 -0700

    adding test skeletons

---
+++ tests/applications/quench_processing/test_quench_linac.py
@@ -0,0 +1,63 @@
+from unittest import TestCase
+
+
+class TestQuenchCavity(TestCase):
+    def test_current_q_loaded_pv_obj(self):
+
+        self.fail()
+
+    def test_hw_mode_pv_obj(self):
+
+        self.fail()
+
+    def test_hw_mode(self):
+
+        self.fail()
+
+    def test_quench_latch_pv_obj(self):
+
+        self.fail()
+
+    def test_quench_latch_invalid(self):
+
+        self.fail()
+
+    def test_quench_intlk_bypassed(self):
+
+        self.fail()
+
+    def test_fault_waveform_pv_obj(self):
+
+        self.fail()
+
+    def test_fault_time_waveform_pv_obj(self):
+
+        self.fail()
+
+    def test_reset_interlocks(self):
+
+        self.fail()
+
+    def test_walk_to_quench(self):
+
+        self.fail()
+
+    def test_wait(self):
+
+        self.fail()
+
+    def test_wait_for_quench(self):
+
+        self.fail()
+
+    def test_check_abort(self):
+
+        self.fail()
+
+    def test_quench_process(self):
+
+        self.fail()
+
+    def test_validate_quench(self):
+
+        self.fail()
