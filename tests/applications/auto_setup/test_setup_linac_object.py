fd293bbc59ce2d6fc5031fe6bb1c325e8ff59bd3
Lisa Zacarias <zacarias@slac.stanford.edu>
2024-10-02 16:55:00 -0700

    adding skeleton of setup object tests

---
+++ tests/applications/auto_setup/test_setup_linac_object.py
@@ -0,0 +1,75 @@
+from unittest import TestCase
+
+
+class TestSetupLinacObject(TestCase):
+    def test_pv_prefix(self):
+        self.fail()
+
+    def test_auto_pv_addr(self):
+        self.fail()
+
+    def test_start_pv_obj(self):
+        self.fail()
+
+    def test_stop_pv_obj(self):
+        self.fail()
+
+    def test_shutoff_pv_obj(self):
+        self.fail()
+
+    def test_abort_pv_obj(self):
+        self.fail()
+
+    def test_abort_requested(self):
+        self.fail()
+
+    def test_clear_abort(self):
+        self.fail()
+
+    def test_trigger_setup(self):
+        self.fail()
+
+    def test_trigger_shutdown(self):
+        self.fail()
+
+    def test_request_abort(self):
+        self.fail()
+
+    def test_kill_setup(self):
+        self.fail()
+
+    def test_ssa_cal_requested_pv_obj(self):
+        self.fail()
+
+    def test_ssa_cal_requested(self):
+        self.fail()
+
+    def test_ssa_cal_requested(self):
+        self.fail()
+
+    def test_auto_tune_requested_pv_obj(self):
+        self.fail()
+
+    def test_auto_tune_requested(self):
+        self.fail()
+
+    def test_auto_tune_requested(self):
+        self.fail()
+
+    def test_cav_char_requested_pv_obj(self):
+        self.fail()
+
+    def test_cav_char_requested(self):
+        self.fail()
+
+    def test_cav_char_requested(self):
+        self.fail()
+
+    def test_rf_ramp_requested_pv_obj(self):
+        self.fail()
+
+    def test_rf_ramp_requested(self):
+        self.fail()
+
+    def test_rf_ramp_requested(self):
+        self.fail()
