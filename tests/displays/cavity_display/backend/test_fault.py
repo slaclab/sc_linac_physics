42358b337be58aade6a365acf8e4dfde5955d22f
Haley <haleym@slac.stanford.edu>
2024-10-29 16:54:36 -0700

    Resolved merge conflicts

--- tests/displays/cavity_display/backend/test_fault.py
+++ tests/displays/cavity_display/backend/test_fault.py
@@ -1,20 +1,28 @@
 from unittest import TestCase, mock
 from unittest.mock import MagicMock, patch
+from random import randint
+from datetime import datetime
+
+from displays.cavity_display.backend.fault import FaultCounter
+
 from lcls_tools.common.controls.pyepics.utils import (
     make_mock_pv,
     EPICS_INVALID_VAL,
     PVInvalidError,
 )
 from lcls_tools.common.data.archiver import ArchiverValue, ArchiveDataHandler
-from datetime import datetime
 
 archiver_value = ArchiverValue()
 get_data_at_time_mock = MagicMock(return_value={"PV": archiver_value})
-get_values_over_time_range_mock = MagicMock(return_value={"PV": ArchiveDataHandler([archiver_value])})
+get_values_over_time_range_mock = MagicMock(
+    return_value={"PV": ArchiveDataHandler([archiver_value])}
+)
 
-@patch.multiple('lcls_tools.common.data.archiver',
-                get_data_at_time=get_data_at_time_mock,
-                get_values_over_time_range=get_values_over_time_range_mock)
+@patch.multiple(
+    'lcls_tools.common.data.archiver',
+    get_data_at_time=get_data_at_time_mock,
+    get_values_over_time_range=get_values_over_time_range_mock
+)
 
 class TestFault(TestCase):
     def setUp(self):
@@ -72,3 +80,48 @@ class TestFault(TestCase):
         from lcls_tools.common.data.archiver import get_data_at_time, get_values_over_time_range
         self.assertEqual(get_data_at_time, get_data_at_time_mock)
         self.assertEqual(get_values_over_time_range, get_values_over_time_range_mock)
+
+
+class TestFaultCounter(TestCase):
+    def setUp(self):
+        max_rand_count = 1
+        self.ok_count = randint(0, max_rand_count)
+        self.fault_count = randint(0, max_rand_count)
+        self.invalid_count = randint(0, max_rand_count)
+
+        self.fault_counter = FaultCounter(
+            ok_count=self.ok_count,
+            fault_count=self.fault_count,
+            invalid_count=self.invalid_count,
+        )
+
+        self.ok_count2 = randint(0, max_rand_count)
+        self.fault_count2 = randint(0, max_rand_count)
+        self.invalid_count2 = randint(0, max_rand_count)
+
+        self.fault_counter2 = FaultCounter(
+            ok_count=self.ok_count2,
+            fault_count=self.fault_count2,
+            invalid_count=self.invalid_count2,
+        )
+
+    def test_sum_fault_count(self):
+        self.assertEqual(
+            self.fault_count + self.invalid_count, self.fault_counter.sum_fault_count
+        )
+
+    def test_ratio_ok(self):
+        self.skipTest("Not yet implemented")
+
+    def test_gt(self):
+        if self.fault_counter.sum_fault_count > self.fault_counter2.sum_fault_count:
+            self.assertTrue(self.fault_counter > self.fault_counter2)
+        else:
+            self.assertFalse(self.fault_counter > self.fault_counter2)
+
+    def test_eq(self):
+        if self.fault_counter.sum_fault_count == self.fault_counter2.sum_fault_count:
+            self.assertTrue(self.fault_counter == self.fault_counter2)
+        else:
+            self.assertFalse(self.fault_counter == self.fault_counter2)
+
