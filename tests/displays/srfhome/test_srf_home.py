1e6d317ea3b7ff4be77a2519323a91d2feebdf2a
Lisa Zacarias <zacarias@slac.stanford.edu>
2024-11-04 17:25:39 -0800

    trying to patch tests

--- tests/displays/srfhome/test_srf_home.py
+++ tests/displays/srfhome/test_srf_home.py
@@ -1,10 +1,8 @@
-import pytest
 from pytestqt.qtbot import QtBot
 
 from displays.srfhome.srf_home import SRFHome
 
 
-@pytest.mark.skip(reason="This test hangs for some reason")
 def test_launches(qtbot: QtBot):
     gui = SRFHome()
     qtbot.addWidget(gui)
