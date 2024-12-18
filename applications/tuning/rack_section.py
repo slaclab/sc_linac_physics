68d7dbc5a4ad8d74df333028d9ca3790b26a1879
Lisa Zacarias <zacarias@slac.stanford.edu>
2024-12-17 17:14:32 -0800

    added legend back in after converting to archiver plot

--- applications/tuning/rack_section.py
+++ applications/tuning/rack_section.py
@@ -13,14 +13,17 @@ from utils.sc_linac.rack import Rack
 class RackSection:
     def __init__(self, rack: Rack):
         self.detune_plot: PyDMArchiverTimePlot = PyDMArchiverTimePlot()
-        self.detune_plot.addAxis(name="detune", orientation="left", plot_data_item=None)
-        self.detune_plot.addAxis(name="steps", orientation="right", plot_data_item=None)
+        self.detune_plot.addAxis(
+            name="detune", orientation="left", plot_data_item=None, label="Detune (Hz)"
+        )
+        self.detune_plot.addAxis(
+            name="steps", orientation="right", plot_data_item=None, label="Steps"
+        )
         self.detune_plot.setTimeSpan(3600)
         self.detune_plot.updateMode = updateMode.AtFixedRate
         self.detune_plot.setPlotTitle("Cavity Detunes")
         self.rack = rack
         self.populate_detune_plot()
-        self.detune_plot.showLegend = True
         rack_file = f"/usr/local/lcls/tools/edm/display/llrf/rf_srf_freq_scan_rack{rack.rack_name}.edl"
         self.edm_screen: PyDMEDMDisplayButton = PyDMEDMDisplayButton(filename=rack_file)
         self.edm_screen.setText("EDM Rack Screen")
@@ -53,23 +56,34 @@ class RackSection:
         for idx, (detune_pv, df_cold_pv, step_pv) in enumerate(detune_pvs):
             r, g, b, alpha = colors[idx]
             rga_color = QColor(r, g, b, alpha)
-            self.detune_plot.addYChannel(
-                y_channel=detune_pv,
-                useArchiveData=True,
-                color=rga_color,
-                yAxisName="detune",
+            self.detune_plot.addLegendItem(
+                self.detune_plot.addYChannel(
+                    y_channel=detune_pv,
+                    useArchiveData=True,
+                    color=rga_color,
+                    yAxisName="detune",
+                ),
+                detune_pv,
             )
-            self.detune_plot.addYChannel(
-                y_channel=df_cold_pv,
-                useArchiveData=True,
-                color=rga_color,
-                lineStyle=QtCore.Qt.DashLine,
-                yAxisName="detune",
+            self.detune_plot.addLegendItem(
+                self.detune_plot.addYChannel(
+                    y_channel=df_cold_pv,
+                    useArchiveData=True,
+                    color=rga_color,
+                    lineStyle=QtCore.Qt.DashLine,
+                    yAxisName="detune",
+                ),
+                df_cold_pv,
             )
-            self.detune_plot.addYChannel(
-                y_channel=step_pv,
-                useArchiveData=True,
-                color=rga_color,
-                lineStyle=QtCore.Qt.DotLine,
-                yAxisName="steps",
+            self.detune_plot.addLegendItem(
+                self.detune_plot.addYChannel(
+                    y_channel=step_pv,
+                    useArchiveData=True,
+                    color=rga_color,
+                    lineStyle=QtCore.Qt.DotLine,
+                    yAxisName="steps",
+                ),
+                step_pv,
             )
+
+        self.detune_plot.setShowLegend(True)
