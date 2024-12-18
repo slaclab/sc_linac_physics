31f1d407fc449369d7e398d3a80c8d927674bb0d
Lisa Zacarias <zacarias@slac.stanford.edu>
2024-12-17 17:01:21 -0800

    trying to add axes

--- applications/tuning/rack_section.py
+++ applications/tuning/rack_section.py
@@ -2,7 +2,7 @@ from PyQt5 import QtCore
 from PyQt5.QtGui import QColor
 from PyQt5.QtWidgets import QGroupBox, QVBoxLayout, QGridLayout
 from edmbutton import PyDMEDMDisplayButton
-from pydm.widgets import PyDMTimePlot
+from pydm.widgets import PyDMArchiverTimePlot
 from pydm.widgets.timeplot import updateMode
 
 from applications.tuning.cavity_section import CavitySection
@@ -12,7 +12,9 @@ from utils.sc_linac.rack import Rack
 
 class RackSection:
     def __init__(self, rack: Rack):
-        self.detune_plot: PyDMTimePlot = PyDMTimePlot()
+        self.detune_plot: PyDMArchiverTimePlot = PyDMArchiverTimePlot()
+        self.detune_plot.addAxis(name="detune", orientation="left", plot_data_item=None)
+        self.detune_plot.addAxis(name="steps", orientation="right", plot_data_item=None)
         self.detune_plot.setTimeSpan(3600)
         self.detune_plot.updateMode = updateMode.AtFixedRate
         self.detune_plot.setPlotTitle("Cavity Detunes")
@@ -40,19 +42,34 @@ class RackSection:
     def populate_detune_plot(self):
         detune_pvs = []
         for cavity in self.rack.cavities.values():
-            detune_pvs.append((cavity.detune_chirp_pv, cavity.df_cold_pv))
+            detune_pvs.append(
+                (
+                    cavity.detune_chirp_pv,
+                    cavity.df_cold_pv,
+                    cavity.stepper_tuner.step_signed_pv,
+                )
+            )
         colors = make_rainbow(len(detune_pvs))
-        for idx, (detune_pv, df_cold_pv) in enumerate(detune_pvs):
+        for idx, (detune_pv, df_cold_pv, step_pv) in enumerate(detune_pvs):
             r, g, b, alpha = colors[idx]
             rga_color = QColor(r, g, b, alpha)
             self.detune_plot.addYChannel(
                 y_channel=detune_pv,
                 useArchiveData=True,
                 color=rga_color,
+                yAxisName="detune",
             )
             self.detune_plot.addYChannel(
                 y_channel=df_cold_pv,
                 useArchiveData=True,
                 color=rga_color,
                 lineStyle=QtCore.Qt.DashLine,
+                yAxisName="detune",
+            )
+            self.detune_plot.addYChannel(
+                y_channel=step_pv,
+                useArchiveData=True,
+                color=rga_color,
+                lineStyle=QtCore.Qt.DotLine,
+                yAxisName="steps",
             )
