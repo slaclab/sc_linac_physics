b61d5576ce76a4b88e60509a3b54498c10a6020a
Haley <haleym@slac.stanford.edu>
2025-03-24 16:23:47 -0700

    Fixed FFT plot to better match the original graph and put a monkey patch for PyQtGraph compatibility with PyQt5 (fixes AttributeError: autoRangeEnabled).

--- applications/microphonics/main.py
+++ applications/microphonics/main.py
@@ -1,3 +1,14 @@
+import pyqtgraph as pg
+
+# Monkey patch for compatibility w/ newer PyQtGraph versions
+if not hasattr(pg.PlotWidget, 'autoRangeEnabled'):
+    def autoRangeEnabled(self):
+        vb = self.plotItem.vb
+        return (vb.state['autoRange'][0], vb.state['autoRange'][1])
+
+
+    pg.PlotWidget.autoRangeEnabled = autoRangeEnabled
+
 import signal
 import sys
 
