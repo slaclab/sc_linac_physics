c133212cf92cf5ed9cf7a4243a841aa1a033d8e7
Haley <haleym@slac.stanford.edu>
2025-05-15 22:20:22 -0700

    Refactor: Simplify channel selection to DF only

--- applications/microphonics/components/components.py
+++ applications/microphonics/components/components.py
@@ -1,91 +1,56 @@
 from pathlib import Path
 
-from PyQt5.QtCore import pyqtSignal, Qt
+from PyQt5.QtCore import pyqtSignal
 from PyQt5.QtWidgets import (
-    QVBoxLayout, QGroupBox, QLabel, QCheckBox, QPushButton,
-    QGridLayout, QFileDialog
+    QVBoxLayout, QGroupBox, QLabel, QPushButton,
+    QGridLayout, QFileDialog, QCheckBox
 )
 
 
 class ChannelSelectionGroup(QGroupBox):
     """
-    UI component that lets people pick which data channels they want to measure.
+    UI component that shows what data channels will be measured.
 
     Component splits channels into two types:
-    - Primary channels (DAC and DF): Always on and can't be turned off
-    - Optional channels: Extra measurements users can toggle on/off as needed
+    - Primary channels (DF): Always on and can't be turned off
     """
+    FIXED_CHANNELS = {
+        'DF': {'label': "DF (Detune Frequency)", 'default_state': True, 'enabled': False}
+    }
 
     def __init__(self, parent=None):
         super().__init__("Channel Selection", parent)
+        # Dictionary stores the QCheckbox widgets, keyed by channel name
+        self.channel_widgets = {}
         self.setup_ui()
 
     def setup_ui(self):
         layout = QGridLayout()
-
-        # Primary channels section
-        primary_label = QLabel("Primary Channels:")
-        layout.addWidget(primary_label, 0, 0)
-
-        self.dac_check = QCheckBox("DAC")
-        self.dac_check.setChecked(True)
-        self.dac_check.setEnabled(False)
-        layout.addWidget(self.dac_check, 0, 1)
-
-        self.df_check = QCheckBox("DF")
-        self.df_check.setChecked(True)
-        self.df_check.setEnabled(False)
-        layout.addWidget(self.df_check, 0, 2)
-
-        # Optional channels toggle
-        self.optional_toggle = QCheckBox("Show Optional Channels")
-        self.optional_toggle.stateChanged.connect(self._toggle_optional_channels)
-        layout.addWidget(self.optional_toggle, 1, 0, 1, 3)
-
-        # Optional channels section (initially hidden)
-        self.optional_group = QGroupBox("Optional Channels")
-        optional_layout = QGridLayout()
-
-        # Keep track of optional channels
-        self.optional_channels = {
-            'AINEG': QCheckBox("AINEG"),
-            'AVDIFF': QCheckBox("AVDIFF"),
-            'AIPOS': QCheckBox("AIPOS"),
-            'ADRV': QCheckBox("ADRV"),
-            'BINEG': QCheckBox("BINEG"),
-            'BVDIFF': QCheckBox("BVDIFF"),
-            'BIPOS': QCheckBox("BIPOS"),
-            'BDRV': QCheckBox("BDRV")
-        }
-
-        # Arrange optional channels
-        row = 0
-        col = 0
-        for name, checkbox in self.optional_channels.items():
-            optional_layout.addWidget(checkbox, row, col)
-            col += 1
-            if col > 2:
-                col = 0
-                row += 1
-
-        self.optional_group.setLayout(optional_layout)
-        layout.addWidget(self.optional_group, 2, 0, 1, 3)
-        self.optional_group.hide()
-
         self.setLayout(layout)
+        current_row = 0
 
-    def _toggle_optional_channels(self, state):
-        """This will show/hide the optional channels based on toggle state"""
-        if state == Qt.Checked:
-            self.optional_group.show()
-        else:
-            self.optional_group.hide()
+        # Create checkboxes for FIXED_CHANNELS
+        if self.FIXED_CHANNELS:
+            col_idx = 0
+            for name, config in self.FIXED_CHANNELS.items():
+                checkbox = QCheckBox(config['label'])
+                checkbox.setChecked(config['default_state'])
+                checkbox.setEnabled(config['enabled'])
+                self.channel_widgets[name] = checkbox
+                layout.addWidget(checkbox, current_row, col_idx)
+                col_idx += 1
+            if col_idx > 0:
+                current_row += 1
+                
+        # Adding stretch to push all UI elements to the top.
+        layout.setRowStretch(current_row, 1)
 
     def get_selected_channels(self):
-        """This will get a list of selected channel names"""
-        channels = ['DAC', 'DF']
-        for name, checkbox in self.optional_channels.items():
-            if checkbox.isChecked():
+        """This will get a list of selected channel names by reading current state
+        of managed QCheckBox widgets."""
+        channels = []
+        for name, checkbox_widget in self.channel_widgets.items():
+            if checkbox_widget.isChecked():
                 channels.append(name)
         return channels
 
