985b50ab810dd10525c3bab270ed601fd470e140
vlotran <vytran@slac.stanford.edu>
2025-06-18 14:02:32 -0700

    removed prefix generation from setup_cavity.py

--- applications/auto_setup/backend/setup_cavity.py
+++ applications/auto_setup/backend/setup_cavity.py
@@ -36,11 +36,7 @@ class SetupCavity(Cavity, SetupLinacObject):
         self.note_pv: str = self.auto_pv_addr("NOTE")
         self._note_pv_obj: Optional[PV] = None
 
-        prefix = "RFS1" if self.number % 4 in (1, 2) else "RFS2"
-        rack = self.rack.rack_name
-        prefix = f"{prefix}{rack}"
-
-        self.tone_count_pv = self.pv_addr(f"{prefix}:DAC_AMPLITUDE")
+        self.tone_count_pv = self.pv_addr(f"{self.rfs_addr}:DAC_AMPLITUDE")
         self._tone_count_pv_obj: Optional[PV] = None
 
     def capture_acon(self):
