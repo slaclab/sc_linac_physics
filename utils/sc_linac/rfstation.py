a283a0982f0be38ff67d6cc0936f76070f0938db
vlotran <vytran@slac.stanford.edu>
2025-07-02 11:45:10 -0700

    rfs1 & rfs2 PVs reduced to just rfs

--- utils/sc_linac/rfstation.py
+++ utils/sc_linac/rfstation.py
@@ -22,40 +22,23 @@ class RFStation(SCLinacObject):
         self._pv_prefix = self.rack.cryomodule.pv_addr(
             f"RFS{self.num}{self.rack.rack_name}:")
 
-        self.rfs1_dac_amp_pv: str = self.pv_addr("DAC_AMPLITUDE")
-        self._rfs1_dac_amp_pv_obj: Optional[PV] = None
-
-        self.rfs2_dac_amp_pv: str = self.pv_addr("DAC_AMPLITUDE")
-        self._rfs2_dac_amp_pv_obj: Optional[PV] = None
+        self.rfs_dac_amp_pv: str = self.pv_addr("DAC_AMPLITUDE")
+        self._rfs_dac_amp_pv_obj: Optional[PV] = None
 
     @property
     def pv_prefix(self):
         return self._pv_prefix
 
     @property
-    def rfs1_dac_amp_pv_obj(self) -> PV:
-        if not self._rfs1_dac_amp_pv_obj:
-            self._rfs1_dac_amp_pv_obj = PV(self.rfs1_dac_amp_pv)
-        return self._rfs1_dac_amp_pv_obj
+    def rfs_dac_amp_pv_obj(self) -> PV:
+        if not self._rfs_dac_amp_pv_obj:
+            self._rfs_dac_amp_pv_obj = PV(self.rfs_dac_amp_pv)
+        return self._rfs_dac_amp_pv_obj
 
     @property
-    def rfs1_dac_amp(self) -> float:
-        return self.rfs1_dac_amp_pv_obj.get()
+    def rfs_dac_amp(self) -> float:
+        return self.rfs_dac_amp_pv_obj.get()
 
-    @rfs1_dac_amp.setter
-    def rfs1_dac_amp(self, value: float):
-        self.rfs1_dac_amp_pv_obj.put(value)
-
-    @property
-    def rfs2_dac_amp_pv_obj(self) -> PV:
-        if not self._rfs2_dac_amp_pv_obj:
-            self._rfs2_dac_amp_pv_obj = PV(self.rfs2_dac_amp_pv)
-        return self._rfs2_dac_amp_pv_obj
-
-    @property
-    def rfs2_dac_amp(self) -> float:
-        return self.rfs2_dac_amp_pv_obj.get()
-
-    @rfs2_dac_amp.setter
-    def rfs2_dac_amp(self, value: float):
-        self.rfs2_dac_amp_pv_obj.put(value)
+    @rfs_dac_amp.setter
+    def rfs_dac_amp(self, value: float):
+        self.rfs_dac_amp_pv_obj.put(value)
