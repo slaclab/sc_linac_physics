baebcda7b5464781df3e426d0f4cc1f90e9cd7df
vlotran <vytran@slac.stanford.edu>
2025-07-08 19:26:01 -0700

    Created CryoPVGroup to process changes to PVs in JT, heater, and liquid level group)

--- utils/simulation/cryo_service.py
+++ utils/simulation/cryo_service.py
@@ -1,5 +1,7 @@
 from caproto.server import PVGroup, pvproperty, PvpropertyString, PvpropertyBoolEnum
 
+from applications.q0 import q0_utils
+
 
 class HeaterPVGroup(PVGroup):
     setpoint = pvproperty(name="MANPOS_RQST", value=24.0)
@@ -31,6 +33,9 @@ class JTPVGroup(PVGroup):
     man_pos = pvproperty(name="MANPOS_RQST", value=40.0)
     mode_string: PvpropertyString = pvproperty(name="MODE_STRING", value="AUTO")
 
+    def trigger_jt_feedback_script(self):
+        raise NotImplementedError
+
     @man_pos.putter
     async def man_pos(self, instance, value):
         await self.readback.write(value)
@@ -39,9 +44,30 @@ class JTPVGroup(PVGroup):
     async def auto(self, instance, value):
         if value == 1:
             await self.mode.write(1)
-            print(f"Setting JT to auto")
+            await self.mode_string.write("AUTO")
+
+    @manual.putter
+    async def manual(self, instance, value):
+        if value == 1:
+            await self.mode.write(0)
+            await self.mode_string.write("MANUAL")
+
+    @mode.putter
+    async def mode(self, instance, value):
+        if value == 1:
+            await self.trigger_jt_feedback_script()
 
 
 class LiquidLevelPVGroup(PVGroup):
     upstream = pvproperty(name="2601:US:LVL", value=75.0)
     downstream = pvproperty(name="2301:DS:LVL", value=93.0)
+
+
+class CryoPVGroup(JTPVGroup, HeaterPVGroup, LiquidLevelPVGroup):
+    async def trigger_jt_feedback_script(self):
+        starting_ll = self.downstream.value
+        if starting_ll != q0_utils.MAX_DS_LL:
+            target_ll_diff = q0_utils.MAX_DS_LL - self.downstream.value
+            print("Waiting for downstream liquid level to reach 93")
+            await self.downstream.write(starting_ll + target_ll_diff)
+            print(f"Liquic level is at {self.downstream.value}")
