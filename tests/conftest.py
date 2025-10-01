4b59834b9318463940fe8edcb5916f97891ad88b
Lisa Zacarias <zacarias@slac.stanford.edu>
2025-09-30 23:26:48 -0700

    test: trying to fix failing decoder display tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,20 @@
 import os
+import sys
+
+import pytest
+from qtpy.QtWidgets import QApplication
 
 os.environ.setdefault("QT_QPA_PLATFORM", "offscreen")
 os.environ.setdefault("QT_API", "pyqt5")
 # Optional: avoid any PyDM telemetry/tasks
 os.environ.setdefault("PYDM_DISABLE_TELEMETRY", "1")
+
+
+@pytest.fixture(scope="session", autouse=True)
+def qapp_global():
+    if not QApplication.instance():
+        app = QApplication(sys.argv)
+        yield app
+        app.quit()
+    else:
+        yield QApplication.instance()
