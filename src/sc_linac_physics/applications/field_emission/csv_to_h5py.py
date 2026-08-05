57129f39c791f5799de02c35258e006b7214975c
Kvetta <kvetta@slac.stanford.edu>
2026-08-05 13:02:04 -0700

    guarding file overwrite with if name = main

--- src/sc_linac_physics/applications/field_emission/csv_to_h5py.py
+++ src/sc_linac_physics/applications/field_emission/csv_to_h5py.py
@@ -17,38 +17,39 @@ input_csvs = glob.glob(os.path.join(input_path, "*.csv"))
 h5_filename = "field_emission_data.hdf5"
 
 
-with h5py.File(h5_filename, "w") as h5f:
-    for csv_path in input_csvs:
-        csv_name = os.path.basename(csv_path)
+if __name__ == "__main__":
+    with h5py.File(h5_filename, "a") as h5f:
+        for csv_path in input_csvs:
+            csv_name = os.path.basename(csv_path)
 
-        match = re.fullmatch(
-            r"cm(\d+|\w+)_(\d+_\d+_\d+_\d+_\d+)_cavity(\d+)_(\w+)\.csv",
-            csv_name,
-        )
-        if match:
-            cm = match.group(1)
-            raw_date = match.group(2)
-            yy, mm, dd, hh, ss = raw_date.split("_")
-            date = f"20{yy}-{mm}-{dd}_{hh}{ss}"
-            cav = match.group(3)
-            readout_type = match.group(4)
-
-            df = pd.read_csv(csv_path)
-            values = df.drop(columns="timestamps").to_numpy(dtype="float64")
-
-            group = h5f.require_group(f"CM{cm}/{date}/CAV{cav}")
-            group.attrs["cryomodule"] = cm
-            group.attrs["cavity"] = cav
-            group.attrs["date"] = date
-
-            dset = group.create_dataset(
-                f"{readout_type}", data=values, compression="gzip"
+            match = re.fullmatch(
+                r"cm(\d+|\w+)_(\d+_\d+_\d+_\d+_\d+)_cavity(\d+)_(\w+)\.csv",
+                csv_name,
             )
-            no_time_df = df.drop(columns="timestamps")
-            dset.attrs["columns"] = no_time_df.columns.astype(str).tolist()
-            dset.attrs["readout_type"] = readout_type
-            dset.attrs["source_file"] = csv_name
-            dset.attrs["created"] = str(pd.Timestamp.now())
+            if match:
+                cm = match.group(1)
+                raw_date = match.group(2)
+                yy, mm, dd, hh, ss = raw_date.split("_")
+                date = f"20{yy}-{mm}-{dd}_{hh}{ss}"
+                cav = match.group(3)
+                readout_type = match.group(4)
 
-        else:
-            print(f"CSV: {csv_name} has incorrect naming format")
+                df = pd.read_csv(csv_path)
+                values = df.drop(columns="timestamps").to_numpy(dtype="float64")
+
+                group = h5f.require_group(f"CM{cm}/{date}/CAV{cav}")
+                group.attrs["cryomodule"] = cm
+                group.attrs["cavity"] = cav
+                group.attrs["date"] = date
+
+                dset = group.create_dataset(
+                    f"{readout_type}", data=values, compression="gzip"
+                )
+                no_time_df = df.drop(columns="timestamps")
+                dset.attrs["columns"] = no_time_df.columns.astype(str).tolist()
+                dset.attrs["readout_type"] = readout_type
+                dset.attrs["source_file"] = csv_name
+                dset.attrs["created"] = str(pd.Timestamp.now())
+
+            else:
+                print(f"CSV: {csv_name} has incorrect naming format")
